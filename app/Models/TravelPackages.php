3bfab4a05119a5ea5b659f1216df4fdea7126a89
andmalt <andrea.maltempi@gmail.com>
2021-11-12 15:47:52 +0100

    created travel packages table and model

--- app/Models/TravelPackages.php
+++ app/Models/TravelPackages.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class TravelPackages extends Model
 {
-    //
+    protected $fillables = ['name','locality','date_from','date_to','guests','rooms','amenities','rating','description','price'];
 }
